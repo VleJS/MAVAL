bd35e87e8348018e0e97f8e65adb335467040cd5
VleJS <102659298+VleJS@users.noreply.github.com>
2023-01-12 21:29:24 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+
+
+  const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/gold-site.jpg") {
+    myImage.setAttribute("src", "images/gold-site2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/gold-site.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name",myName);
+    myHeading.textContent = `MAVAL is cool,${myName}`;
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+}else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `MAVAL is cool ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+  };
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `MAVAL is cool, ${myName}`;
+    }
+  }
+  
